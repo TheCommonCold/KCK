77b736dc1f437834d31a68ee1883b2b022e499fb
Luker <luker.plus@gmail.com>
2018-10-20 22:07:34 +0200

    almost calc

--- map.py
+++ map.py
@@ -14,6 +14,12 @@ def openfile():
 def interpolator(hsv1, hsv2, t):
     return (hsv1[0] + (hsv2[0] - hsv1[0]) * t), (hsv1[1] + (hsv2[1] - hsv1[1]) * t), (hsv1[2] + (hsv2[2] - hsv1[2]) * t)
 
+def normalize(x):
+    return (x - min(x)) / (max(x)-min(x))
+
+def calcangle(a):
+    b=(-0.157991, -0.789953, -0.592464)
+    a[]
 def normalizer(data):
     temp=[]
     for array in data:
@@ -24,14 +30,17 @@ def normalizer(data):
     data=[[gradient((x-minimum)/(maximum-minimum))for x in array] for array in data]
     return data
 
+def crossvectors(a,b,c,d):
+    return normalize(np.cross(a,b)+np.cross(b,c)+np.cross(c,d)+np.cross(d,a))
+
 def normalcalculator(data):
     normals=[]
     for i in range(1,len(data)-1):
         temparray=[]
         for j in range(1,len(data[0])-1):
-            data[i][j]
-
-
+            temparray.append(crossvectors((75.31,0,data[i][j+1]-data[i][j]),(0,75.31,data[i-1][j]-data[i][j]),(-75.31,0,data[i][j-1]-data[i][j]),(0,-75.31,data[i+1][j]-data[i][j])))
+        #print(temparray)
+        normals.append(temparray);
 def gradient(x):
     colour1=(10/30,1,0.8)
     colour2=(6/36,1,1)
@@ -40,6 +49,9 @@ def gradient(x):
         return interpolator(hsv_to_rgb(colour1), hsv_to_rgb(colour2), (x*2))
     else:
         return interpolator(hsv_to_rgb(colour2), hsv_to_rgb(colour3), (x - 1/2) * 2)
+
+def shade(data):
+    sun=(-0.157991, -0.789953, -0.592464)
 
 
 
@@ -50,6 +62,7 @@ if __name__ == '__main__':
     #print(img)
     #plt.colorbar()
 
+    normalcalculator(data)
     data = normalizer(data)
     plt.imshow(data)
     plt.show()
