8d6a41c72a5a95e9b9ceb709b1dcbb35a65df121
Luker <luker.plus@gmail.com>
2018-10-24 22:10:20 +0200

    does not work

--- jets3.py
+++ jets3.py
@@ -8,13 +8,22 @@ from skimage.morphology import disk
 import skimage.morphology as mp
 from skimage import util
 from skimage.color import rgb2hsv, hsv2rgb, rgb2gray,gray2rgb
-from skimage.filters.edges import convolve
+from skimage.filters import *
 from matplotlib import pylab as plt
 import numpy as np
 from numpy import array
+io.use_plugin('matplotlib')
+
+nazwy=["samolot0"+str(i)+".jpg" for i in range(9)]
+#nazwy=nazwy+["samolot"+str(i)+".jpg" for i in range(10,21)]
 
 def imageprocessor(data):
-    data = rgb2gray(data)
+    data =[[(x[0]) for x in array] for array in data]
+    K=np.array([[1,1,1],
+       [1, 1, 1],
+       [1, 1, 1]])
+    K=K/10
+    data=filters.convolve(data,K)
     data = (filters.sobel(data))
     data= mp.dilation(data)
     data=mp.dilation(data)
@@ -28,11 +37,23 @@ def imagecompiler(data1,data2):
                 data1[i][j][0]=0
                 data1[i][j][1]=1
                 data1[i][j][2] = data1[i][j][2]+data2[i][j]
-    return hsv2rgb(data1)
+    return data1
+
 
 if __name__ == '__main__':
-    data=io.imread("samolot11.jpg")
-    data = img_as_float(data)
-    data=imagecompiler(data,imageprocessor(data))
-    io.imshow(data)
+    finaldata=[]
+    columns = 3
+    rows = 3
+    fig = plt.figure(figsize=(20, 20))
+    i = 1
+    for nazwa in nazwy:
+        print(nazwa)
+        data = io.imread(nazwa)
+        data = img_as_float(data)
+        #data = imagecompiler(data, imageprocessor(data))
+        data=imageprocessor(data)
+        #data=matplotlib.colors.hsv_to_rgb(data)
+        fig.add_subplot(rows, columns, i)
+        plt.imshow(data)
+        i=i+1
     io.show()
